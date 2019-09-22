1237fe730d29991960168973d45571dec18a78b8
Rob <rm.rf.etc@gmail.com>
2019-09-22 12:20:48 -0700

    Fix util.match

--- packages/util/match.js
+++ packages/util/match.js
@@ -1,4 +1,4 @@
 export default (...exps) => {
     const result = exps.find((exp) => exp[0]);
-    if (result && typeof result[1] === "function") result[1]();
+    if (result && typeof result[1] === "function") return result[1]();
 };
