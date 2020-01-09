93b044cdc5e7ac6c3ce5e4ebcfbca2e82da7c59f
Rob <rob.christian@figure-eight.com>
2020-01-09 02:38:29 -0800

    Cleanup react test app

--- packages/react-test-ground/src/App.js
+++ packages/react-test-ground/src/App.js
@@ -4,33 +4,27 @@ import store from "./store";
 import useSharedState from "use-simple-shared-state";
 
 const selectors = [
-  // (state) => state.counters.count1,
-  // (state) => state.counters.count2,
+  (state) => state.counters.count1,
   (state) => state.todos,
 ];
 
 const {
-  // setCounter1,
-  // setCounter2,
-  // incrementCounter1,
-  // decrementCounter1,
-  // incrementCounter2,
-  // decrementCounter2,
+  setCounter1,
+  incrementCounter1,
+  decrementCounter1,
   pushTodo,
   popTodo,
   removeTodo,
 } = store.actions;
 
 const App = () => {
-  // const [count1, count2, todos] = useSharedState(store, selectors);
-  const [todos] = useSharedState(store, selectors);
-  // const [field1, setField1] = useState(0);
-  // const [field2, setField2] = useState(0);
+  const [count1, todos] = useSharedState(store, selectors);
+  const [field1, setField1] = useState(0);
   const [field3, setField3] = useState("");
 
   return (
     <div className="App">
-      {/* <div>
+      <div>
         <span>{count1}</span>
         <button onClick={incrementCounter1}>+</button>
         <button onClick={decrementCounter1}>-</button>
@@ -39,15 +33,6 @@ const App = () => {
           <button onClick={() => setCounter1(field1)}>Set</button>
         </div>
       </div>
-      <div>
-        <span>{count2}</span>
-        <button onClick={incrementCounter2}>+</button>
-        <button onClick={decrementCounter2}>-</button>
-        <div>
-          <input type="text" value={field2} onChange={({ target }) => setField2(+target.value)}></input>
-          <button onClick={() => setCounter2(field2)}>Set</button>
-        </div>
-      </div> */}
       <div>
         <input type="text" value={field3} onChange={({ target }) => setField3(target.value)}></input>
         <button onClick={() => { pushTodo(field3); setField3(""); }}>Add To-Do</button>
--- packages/react-test-ground/src/store.js
+++ packages/react-test-ground/src/store.js
@@ -16,20 +16,11 @@ const actions = ({ getState }) => ({
 	setCounter1: (value) => ({
 		counters: { count1: +value },
 	}),
-	setCounter2: (value) => ({
-		counters: { count2: +value },
-	}),
 	incrementCounter1: () => ({
 		counters: { count1: getState(s => s.counters.count1) + 1 },
 	}),
 	decrementCounter1: () => ({
 		counters: { count1: getState(s => s.counters.count1) - 1 },
-	}),
-	incrementCounter2: () => ({
-		counters: { count2: getState(s => s.counters.count2) + 1 },
-	}),
-	decrementCounter2: () => ({
-		counters: { count2: getState(s => s.counters.count2) - 1 },
 	}),
 	pushTodo: (label) => {
 		const len = getState(s => s.todos.length);
